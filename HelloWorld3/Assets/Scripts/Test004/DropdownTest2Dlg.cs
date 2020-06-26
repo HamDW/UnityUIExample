d1f215589f4761df838e751c1ec90ecfd2faf057
hamdw <hebong2@gmail.com>
2020-06-27 02:09:00 +0900

    ScrollViewTest 추가

---
+++ HelloWorld3/Assets/Scripts/Test004/DropdownTest2Dlg.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownTest2Dlg : MonoBehaviour
+{
+    public static string[] cCityList = { "서울", "광주", "대전", "부산", "전주" };
+    [SerializeField] Dropdown m_Dropdown;
+    [SerializeField] Text m_txtResult;
+    [SerializeField] Button m_btnResult;
+    [SerializeField] Button m_btnClear;
+
+    [SerializeField] List<Button> m_Items;
+
+
+    private List<string> m_listData = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Dropdown.onValueChanged.AddListener(delegate {
+            OnValueChanged_CityList(m_Dropdown);
+        });
+        m_btnResult.onClick.AddListener(OnClicked_Result);
+        m_btnClear.onClick.AddListener(OnClicked_Clear);
+
+        Initialize1();
+    }
+
+    private void Initialize1()
+    {
+        m_listData.Add("서울");
+        m_listData.Add("광주");
+        m_listData.Add("대전");
+        m_listData.Add("부산");
+        m_listData.Add("전주");
+
+        m_Dropdown.AddOptions(m_listData);
+    }
+
+    public void OnValueChanged_CityList(Dropdown kDropdown)
+    {
+        int nPos = kDropdown.value;
+        string sCity = m_listData[nPos];
+
+        m_txtResult.text = nPos + " : " + sCity;
+    }
+
+
+    public void OnClicked_Result()
+    {
+        int nPos = m_Dropdown.value;
+        string sCity = m_listData[nPos];
+        string sResult = "당신이 이동할 도시는 " + sCity + "입니다. ";
+        m_txtResult.text = sResult;
+    }
+
+    public void OnClicked_Clear()
+    {
+        m_txtResult.text = "초기화 됐습니다.";
+        m_Dropdown.value = 0;
+    }
+}
--- HelloWorld3/Assets/Scripts/Test004/ScrollViewTestDlg.cs
+++ HelloWorld3/Assets/Scripts/Test004/ScrollViewTestDlg.cs
@@ -1,18 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 
 public class ScrollViewTestDlg : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static string[] cCityList = { "서울", "광주", "대전", "부산", "전주", "대구", "인천" };
+    [SerializeField] ScrollRect m_ScrollRect = null;
+    [SerializeField] Text m_txtResult = null;
+    [SerializeField] Button m_btnResult = null;
+    [SerializeField] Button m_btnClear = null;
+
+    [SerializeField] GameObject m_prefabItem = null;
+
+    private List<ItemSlot> m_listItem = new List<ItemSlot>();
+
+    private int m_iSelectIndex = 0;
+
+
     void Start()
     {
+        m_btnResult.onClick.AddListener(OnClicked_Result);
+        m_btnClear.onClick.AddListener(OnClicked_Clear);
+
+        m_ScrollRect.onValueChanged.AddListener( (Vector2 value) =>{
+            OnValueChanged_CityList(value);
+        });
+
+
+        Initialize();
+    }
+
+
+
+    public void Initialize()
+    {
+        m_listItem.Clear();
+
+        for( int i = 0; i < cCityList.Length; i++ )
+        {
+            GameObject go = Instantiate(m_prefabItem, m_ScrollRect.content) as GameObject;
+            ItemSlot kItem = go.GetComponent<ItemSlot>();
+
+            kItem.transform.localScale = new Vector3(1, 1, 1);  // 반드시 할것
+            kItem.Initialize(i, cCityList[i]);
+            
+            Button btn = kItem.GetComponent<Button>();
+            int idx = i;
+            btn.onClick.AddListener(() => {
+                OnClicked_SelectItem(idx);
+            });
+
+            m_listItem.Add(kItem);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClicked_SelectItem(int iIndex)
     {
+        ClearAllSelectedItem();
+        ItemSlot kItem = m_listItem[iIndex];
+        kItem.SetSelect(true);
+
+        m_iSelectIndex = iIndex;
+
+        m_txtResult.text = cCityList[iIndex];
         
+        string sLog = string.Format(" Select Index = {0}", iIndex);
+        Debug.Log(sLog);
+    }
+
+    public void ClearAllSelectedItem()
+    {
+        for( int i = 0; i < m_listItem.Count; i++ )
+        {
+            m_listItem[i].SetSelect(false);
+        }
+    }
+
+
+
+    public void OnValueChanged_CityList(Vector2 value)
+    {
+        //Debug.Log("Scroll Pos = " + value.x + ", " + value.y);
+    }
+
+
+
+    public void OnClicked_Result()
+    {
+        //int nPos = m_Dropdown.value;
+        string sCity = cCityList[m_iSelectIndex];
+        string sResult = "당신이 이동할 도시는 <color=#aa00ffff>" + sCity + "</color> 입니다. ";
+        m_txtResult.text = sResult;
+    }
+
+    public void OnClicked_Clear()
+    {
+        m_txtResult.text = "초기화 됐습니다.";
+        m_iSelectIndex = 0;
     }
 }
